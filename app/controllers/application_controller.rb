8ee7a9e25b7db40d96eac05389b9305b5160d183
Mihailescu Cristina <mihailescucristina@outlook.com>
2017-03-29 19:44:47 +0200

    Modified controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     
     def hello
-        render html: "hello there!"
+        render html: "hiiiii!"
     end
 end
